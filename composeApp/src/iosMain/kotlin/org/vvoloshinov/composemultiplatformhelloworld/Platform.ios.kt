189e0c7fc5538e4c0c1838d2b80990368cb42375
Vladimir Voloshinov <vlad.voloshinov@gmail.com>
2024-11-12 22:46:18 -0500

    just added some formatting and strings

--- composeApp/src/iosMain/kotlin/org/vvoloshinov/composemultiplatformhelloworld/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/org/vvoloshinov/composemultiplatformhelloworld/Platform.ios.kt
@@ -2,8 +2,9 @@ package org.vvoloshinov.composemultiplatformhelloworld
 
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+class IOSPlatform : Platform {
+    override val name: String =
+        "I am iOS: so - " + UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
